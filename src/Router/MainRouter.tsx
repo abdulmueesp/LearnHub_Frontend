171fc9891c51cc9ec3f41900cde23f4a312ee6a4
abdulmueesp <abdulmueesp5@gmail.com>
2024-10-22 20:12:40 +0530

    Router page created

---
+++ src/Router/MainRouter.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+
+const MainRouter = () => {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default MainRouter
